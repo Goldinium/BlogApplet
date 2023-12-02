8e4d05fa2937585018647b37b755ef3a25a8072a
Goldinium <badrbenslama8@gmail.com>
2023-12-02 21:31:47 +0100

    added missing files

--- db/migrate/create_comments.rb
+++ db/migrate/create_comments.rb
@@ -1,8 +1,9 @@
-class CreateLikes < ActiveRecord::Migration[7.1]
+class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
+    create_table :comments do |t|
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
+      t.text :text
 
       t.timestamps
     end
--- db/migrate/create_likes.rb
+++ db/migrate/create_likes.rb
@@ -1,9 +1,8 @@
-class CreateComments < ActiveRecord::Migration[7.1]
+class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :likes do |t|
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
-      t.text :text
 
       t.timestamps
     end
