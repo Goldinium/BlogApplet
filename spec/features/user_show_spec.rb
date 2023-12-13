14a47f3245cb03c8a1326a0d267d12e7b2aad6ed
Goldinium <badrbenslama8@gmail.com>
2023-12-13 14:16:11 +0100

    fixed tests as requested by the code reviewer

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -9,6 +9,22 @@ RSpec.feature 'UserShowPostsAndNavigation', type: :feature do
     visit user_path(user)
   end
 
+  it 'displays the user profile picture' do
+    expect(page).to have_css("img[src*='#{user.photo}']")
+  end
+
+  it 'displays the username' do
+    expect(page).to have_content(user.name)
+  end
+
+  it 'displays the number of posts the user has written' do
+    expect(page).to have_content("Number of posts: #{user.posts.count}")
+  end
+
+  it 'displays the user bio' do
+    expect(page).to have_content(user.bio)
+  end
+
   it 'displays the first 3 posts' do
     user.three_most_recent_posts.each do |post|
       expect(page).to have_content(post.title)
